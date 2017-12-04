34250084682c82c38a1db59d9fdbd8eeb00290d3
Jacek Podkanski <ruby.object@googlemail.com>
2017-12-04 18:08:18 +0000

    added rubocop.yml and some tweaking

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,6 @@
 class Address < ApplicationRecord
-  belongs_to :postal_code, class_name: 'Postcode',
+  belongs_to :postal_code,
+             class_name: 'Postcode',
              foreign_key: 'postcode',
              primary_key: 'code',
              inverse_of: :addresses
@@ -7,9 +8,8 @@ class Address < ApplicationRecord
   validate :has_valid_postcode
 
   def has_valid_postcode
-    if Postcode.where(code: self.postcode).count.zero?
-      errors.add(:postcode, "is not valid")
-    end
+    return unless Postcode.where(code: postcode).count.zero?
+    errors.add(:postcode, 'is not valid')
   end
 
   def self.bulk_text_import
@@ -18,7 +18,7 @@ class Address < ApplicationRecord
 
     # https://stackoverflow.com/questions/164979/uk-postcode-regex-comprehensive
     postcode_regex = /([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z]))))\s?[0-9][A-Za-z]{2})/
-    fh = File.open(Rails.root.join 'import.txt')
+    fh = File.open Rails.root.join('import.txt')
     fh.readlines.each do |line|
       postcode_regex =~ line
       new_address = Address.create(firstline: $`, postcode: $~.to_s)
